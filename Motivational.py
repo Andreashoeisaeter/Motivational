d893e2bbf8115cb60d000bce1e2eb55fb03e6f08
Andreashoeisaeter <a.hoisater@gmail.com>
2025-04-06 22:16:42 +0200

    Update Motivational.py

--- Motivational.py
+++ Motivational.py
@@ -3,6 +3,13 @@ import datetime
 import schedule
 import time
 import winsound
+from colorama import init, Fore
+
+init()
+
+BLUE = Fore.BLUE
+RESET = Fore.RESET
+YELLOW = Fore.YELLOW
 
 def messages():
 
@@ -10,10 +17,10 @@ def messages():
     duration = 1000
     winsound.Beep(frequency, duration)
     now = datetime.datetime.now()
-    print(now)
+    print(f"{BLUE}{now.strftime("%c")}{RESET}")
     with open("Messages.txt") as f:
             lines = f.readlines()
-    print(random.choice(lines))
+    print(f"{YELLOW}{random.choice(lines)}{RESET}")
 
 messages()
 
@@ -23,4 +30,6 @@ my_schedule.every(1).hours.do(messages)
 while True:
     my_schedule.run_pending()
     time.sleep(1)
+
+
 
