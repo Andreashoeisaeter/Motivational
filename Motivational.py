190e0ae2636424425cdb0bb7e1fba00126e21e6a
Andreashoeisaeter <a.hoisater@gmail.com>
2025-04-08 17:23:51 +0200

    Update Motivational.py

--- Motivational.py
+++ Motivational.py
@@ -4,12 +4,20 @@ import schedule
 import time
 import winsound
 from colorama import init, Fore
+import keyboard
 
 init()
 
 BLUE = Fore.BLUE
 RESET = Fore.RESET
 YELLOW = Fore.YELLOW
+WHITE = Fore.WHITE
+GREEN = Fore.GREEN
+
+print(f"{WHITE}Welcome to Motivational, a quote motivation booster program! {RESET}\n")
+
+print(f"{GREEN}Sending you one message every hour ... {RESET}\n")
+
 
 def messages():
 
@@ -30,6 +38,3 @@ my_schedule.every(1).hours.do(messages)
 while True:
     my_schedule.run_pending()
     time.sleep(1)
-
-
-
